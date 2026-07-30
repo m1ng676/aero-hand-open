51108b3cfc7048436d134e14abcb462761810894
m1ng676 <2780161806@qq.com>
2026-07-30 22:15:08 +0800

    Update run_sequence.py

--- sdk/examples/run_sequence.py
+++ sdk/examples/run_sequence.py
@@ -16,7 +16,7 @@
 from aero_open_sdk.aero_hand import AeroHand
 
 if __name__ == "__main__":
-    hand = AeroHand()
+    hand = AeroHand(""COM3)
 
     ## Create a trajectory for the hand to follow
     trajectory = [
